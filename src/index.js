0f6440562e65e91134bcc47797a2d970c297ce5b
Burbuha <burbovskaja@gmail.com>
2021-01-09 01:00:31 +0300

    feat: implement function that converts given number, to readable string

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+  const num = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+  ];
+  const tens = [
+    "",
+    "",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+  ];
+
+  const numberToWords = (number) => {
+    if (number < 20) return num[number];
+    let digit = number % 10;
+    if (number < 100)
+      return tens[Math.floor(number / 10)] + (digit ? " " + num[digit] : "");
+    if (number < 1000)
+      return (
+        num[Math.floor(number / 100)] +
+        " hundred" +
+        (number % 100 == 0 ? "" : " " + numberToWords(number % 100))
+      );
+    return (
+      numberToWords(Math.floor(number / 1000)) +
+      " thousand" +
+      (number % 1000 != 0 ? "" + numberToWords(number % 1000) : " ")
+    );
+  };
+
+  return numberToWords(number);
+};
